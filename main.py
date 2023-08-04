488455f9a132e78080f5e4f996ec0e26350249f1
Bartłomiej Lis <lisu.b117@gmail.com>
2023-08-04 20:59:24 +0200

    Writing a script

---
+++ main.py
@@ -0,0 +1,24 @@
+#Kalkulator ilości dopasowań z pamięci tłumaczeniowych
+import json
+gvn_jsonfile = open("Analysis #1.json")
+json_data = json.load(gvn_jsonfile)
+#Główny skrypt programu
+def main():
+    print("Kalkulator ilości dopasowań z pamięci tłumaczeniowych \n")
+    project_name = json_data["projectName"]
+    print("Numer zlecenia:", project_name)
+    source_lang = json_data["analyseLanguageParts"][0]["sourceLang"]
+    target_lang = json_data["analyseLanguageParts"][0]["targetLang"]
+    print("Para językowa:", source_lang, "->", target_lang)
+    tm_pages1 = json_data["analyseLanguageParts"][0]["data"]["contextMatch"]["normalizedPages"]
+    tm_pages2 = json_data["analyseLanguageParts"][0]["data"]["match100"]["normalizedPages"]["tm"]
+    tm_pages3 = json_data["analyseLanguageParts"][0]["data"]["match95"]["normalizedPages"]["tm"]
+    tm_pages4 = json_data["analyseLanguageParts"][0]["data"]["match85"]["normalizedPages"]["tm"]
+    tm_pages5 = json_data["analyseLanguageParts"][0]["data"]["match75"]["normalizedPages"]["tm"]
+    all_tm_pages = tm_pages1 + tm_pages2 + tm_pages3 + tm_pages4 + tm_pages5
+    all_pages = json_data["analyseLanguageParts"][0]["data"]["total"]["normalizedPages"]
+    tm_ratio = all_tm_pages / all_pages
+    print("Liczba stron (1800 zzs): " + "%.2f" % all_tm_pages)
+    print("TMs matches: " + "{0:.0%}".format(tm_ratio))
+    wait = input()
+main()
